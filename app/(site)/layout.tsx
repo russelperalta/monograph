8169df78142acd4fd4277ae62482b01fab64e80d
russelperalta <russelp@monark.com>
2026-01-21 16:46:58 -0800

    Updated css

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,6 +1,6 @@
 import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import "./globals.scss";
+// import Footer from './components/Footer/Footer'
+// import "./globals.scss";
 
 export default function SiteLayout({
   children,
